bece20f097ba1a9544cf70e70d8037bdfffdd3f4
Rayssa <rayssa_nan.svl@hotmail.com>
2024-08-21 15:01:45 -0300

    Finalização dos exemplos de lista

--- EstruturaDeDados/EstruturaDeDados/Program.cs
+++ EstruturaDeDados/EstruturaDeDados/Program.cs
@@ -2,8 +2,74 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Minha primeira branch!");
-        Console.WriteLine("Trabalhando com Git\\Github");
-        Console.WriteLine("Mais uma tentativa de trabalhar com Git");
+        // Trabalhando com lista (list)
+        //Criando uma lista
+
+        List<string> frutas = new List<string>();
+
+        // Adicionar itens na lista
+        frutas.Add("Morango");
+        frutas.Add("Manga");
+        frutas.Add("Goiaba");
+        frutas.Add("Uva");
+
+        // Imprimir os itens da lista 
+        frutas.ForEach(Console.WriteLine);
+        Console.WriteLine();
+        //Console.WriteLine("Minha lista de frutas:");
+        //foreach (string fruta in frutas)
+        //{
+        //    Console.WriteLine(fruta);
+        //}
+
+        frutas.ForEach(Console.WriteLine);
+        Console.WriteLine(); // Pula linha em branco
+
+        // Imprimir elemento na posição específica
+        Console.WriteLine("Fruta no índice 2:" + frutas[2]);
+
+        // Inserir um elemento no índice específico
+        frutas.Insert(1, "Maracujá");
+
+        // Imprimindo a lista novamente
+        Console.WriteLine(); // Pula linha em branco
+
+        //Console.WriteLine("Minha lista de frutas:");
+        //foreach (string fruta in frutas)
+        //{
+        //    Console.WriteLine(fruta);
+        //}
+
+        frutas.ForEach(Console.WriteLine);
+        Console.WriteLine();
+        // Alterar um elemento no índice específico
+        frutas[4] = "Pêra";
+        // Imprimindo a lista novamente
+        //Console.WriteLine(); // Pula linha em branco
+        //Console.WriteLine("Minha lista de frutas:");
+        //foreach (string fruta in frutas)
+        //{
+        //    Console.WriteLine(fruta);
+        //}
+
+        frutas.ForEach(Console.WriteLine);
+        Console.WriteLine();
+        // Remover elementos da lista no índice específico
+        frutas.RemoveAt(3);
+
+        // Remover elementos pelo valor do conteúdo
+        frutas.Remove("Morango");
+
+        // Imprimindo a lista novamente
+        //Console.WriteLine(); // Pula linha em branco
+        //Console.WriteLine("Minha lista de frutas:");
+        //foreach (string fruta in frutas)
+        //{
+        //    Console.WriteLine(fruta);
+        //}
+        frutas.ForEach(Console.WriteLine);
+        Console.WriteLine();
+        // Apagar todos os elementos da lista
+        frutas.Clear();
     }
 }
